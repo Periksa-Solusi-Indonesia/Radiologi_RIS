008fde736bf01458869dd797378ed7d1d1c7c328
Sutan Imam Abu Hanifah <sutanimam@windowslive.com>
2025-09-24 21:28:05 +0700

    update config morita

--- radiology-cron/main.py
+++ radiology-cron/main.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 # Path ke folder worklist
 WORKLIST_PATH = "../worklists"
-CONFIG_PATH = "config.json"
+CONFIG_PATH = "config-mirota.json"
 
 # Default configuration
 DEFAULT_CONFIG = {
